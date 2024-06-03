145faaef034e2aa5f7cb25cb10a3cec6ff70bc98
Jiromtrf <mtrfymd@gmail.com>
2024-06-03 17:18:31 +0900

    デプロイ後のファイル読込エラー修正（n回目）

--- app/config.py
+++ app/config.py
@@ -1,27 +1,14 @@
 import os
-import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 SPREADSHEET_DB_ID = os.getenv("SPREADSHEET_DB_ID")
 LINE_NOTIFY_TOKEN = os.getenv("LINE_NOTIFY_TOKEN")
-private_key_content = os.getenv("PRIVATE_KEY_PATH")
+PRIVATE_KEY_PATH = os.getenv("PRIVATE_KEY_PATH")
 
-# デバッグ用のプリント文
-print(f"SPREADSHEET_DB_ID: {SPREADSHEET_DB_ID}")
-print(f"LINE_NOTIFY_TOKEN: {LINE_NOTIFY_TOKEN}")
-print(f"PRIVATE_KEY_PATH: {private_key_content}")
-
-# 環境変数が設定されているか確認
-if not SPREADSHEET_DB_ID or not LINE_NOTIFY_TOKEN or not private_key_content:
+if not SPREADSHEET_DB_ID or not LINE_NOTIFY_TOKEN or not PRIVATE_KEY_PATH:
     raise ValueError("環境変数が正しく設定されていません。")
-
-try:
-    # JSONファイルの内容を一時ファイルに書き込む
-    PRIVATE_KEY_PATH = "service_account.json"
-    with open(PRIVATE_KEY_PATH, "w") as f:
-        json.dump(json.loads(private_key_content), f)
-except json.JSONDecodeError as e:
-    print(f"Error decoding JSON: {e}")
-    raise ValueError("PRIVATE_KEYの内容が無効なJSONです。") from e
 
 scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
