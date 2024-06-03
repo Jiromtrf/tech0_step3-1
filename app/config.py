6c54fe7a27e7a95b091ac354ccb0575d5b4891ec
Jiromtrf <mtrfymd@gmail.com>
2024-06-03 23:22:31 +0900

    修正

--- app/config.py
+++ app/config.py
@@ -9,7 +9,6 @@ SPREADSHEET_DB_ID = st.secrets["SPREADSHEET_DB_ID"]
 LINE_NOTIFY_TOKEN = st.secrets["LINE_NOTIFY_TOKEN"]
 PRIVATE_KEY_PATH = st.secrets["PRIVATE_KEY_PATH"]
 
-PRIVATE_KEY_PATH["private_key"] = PRIVATE_KEY_PATH["private_key"].replace("\\n", "\n")
 
 if not SPREADSHEET_DB_ID or not LINE_NOTIFY_TOKEN or not PRIVATE_KEY_PATH:
     raise ValueError("環境変数が正しく設定されていません。")
