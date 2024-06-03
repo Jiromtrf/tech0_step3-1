6726b64054b2e7d1710d547ca5222056eafac894
Jiromtrf <mtrfymd@gmail.com>
2024-06-03 22:54:59 +0900

    secretsから読込ように修正

--- app/config.py
+++ app/config.py
@@ -7,7 +7,19 @@ load_dotenv()
 
 SPREADSHEET_DB_ID = st.secrets["SPREADSHEET_DB_ID"]
 LINE_NOTIFY_TOKEN = st.secrets["LINE_NOTIFY_TOKEN"]
-PRIVATE_KEY_PATH = json.loads(st.secrets["PRIVATE_KEY_PATH"])
+PRIVATE_KEY_PATH = {
+    "type": st.secrets["PRIVATE_KEY_PATH"]["type"],
+    "project_id": st.secrets["PRIVATE_KEY_PATH"]["project_id"],
+    "private_key_id": st.secrets["PRIVATE_KEY_PATH"]["private_key_id"],
+    "private_key": st.secrets["PRIVATE_KEY_PATH"]["private_key"].replace("\\n", "\n"),
+    "client_email": st.secrets["PRIVATE_KEY_PATH"]["client_email"],
+    "client_id": st.secrets["PRIVATE_KEY_PATH"]["client_id"],
+    "auth_uri": st.secrets["PRIVATE_KEY_PATH"]["auth_uri"],
+    "token_uri": st.secrets["PRIVATE_KEY_PATH"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["PRIVATE_KEY_PATH"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["PRIVATE_KEY_PATH"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["PRIVATE_KEY_PATH"]["universe_domain"]
+}
 
 if not SPREADSHEET_DB_ID or not LINE_NOTIFY_TOKEN or not PRIVATE_KEY_PATH:
     raise ValueError("環境変数が正しく設定されていません。")
