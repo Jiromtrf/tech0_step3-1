a7fc0d0a5c5bfb0a2b409a76c01f334a74211d36
Jiromtrf <mtrfymd@gmail.com>
2024-06-03 22:07:48 +0900

    secretsから読込ように修正

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 SPREADSHEET_DB_ID = os.getenv("SPREADSHEET_DB_ID")
 LINE_NOTIFY_TOKEN = os.getenv("LINE_NOTIFY_TOKEN")
-PRIVATE_KEY_PATH = st.secrets("PRIVATE_KEY_PATH")
+PRIVATE_KEY_PATH = st.secrets["PRIVATE_KEY_PATH"]
 
 if not SPREADSHEET_DB_ID or not LINE_NOTIFY_TOKEN or not PRIVATE_KEY_PATH:
     raise ValueError("環境変数が正しく設定されていません。")
