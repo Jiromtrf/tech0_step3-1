d9034ad61dec9702516a7bf61f3e8a3640f19d27
Jiromtrf <mtrfymd@gmail.com>
2024-06-04 22:31:39 +0900

    削除

--- app/pages/secrets確認用.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def main():
-    st.write("SPREADSHEET_DB_ID:", st.secrets["SPREADSHEET_DB_ID"])
-    st.write("LINE_NOTIFY_TOKEN:", st.secrets["LINE_NOTIFY_TOKEN"])
-    st.write("PRIVATE_KEY_PATH:", st.secrets["PRIVATE_KEY_PATH"])
-
-if __name__ == "__main__":
-    main()
