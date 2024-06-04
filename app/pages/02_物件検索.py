9f04fb8aff6b99b94cd02b7c2a4e0d49465229f5
Jiromtrf <mtrfymd@gmail.com>
2024-06-05 02:59:30 +0900

    デフォルトの設定を調整。

--- app/pages/02_物件検索.py
+++ app/pages/02_物件検索.py
@@ -146,7 +146,7 @@ def remove_favorite_property(username, property_id):
     sheet = client.open_by_key(SPREADSHEET_DB_ID).worksheet("お気に入りDB")
     records = sheet.get_all_records()
     fav_df = pd.DataFrame(records)
-    fav_df = [(fav_df['username'] != username) | (fav_df['property_id'] != property_id)]
+    fav_df = fav_df[(fav_df['username'] != username) | (fav_df['property_id'] != property_id)]
     sheet.clear()
     sheet.append_row(["username", "property_id"])
     for index, row in fav_df.iterrows():
@@ -173,12 +173,12 @@ def main():
         price_min, price_max = st.slider(
             '■ 家賃範囲 (万円)',
             min_value=float(1),
-            max_value=float(df['家賃'].max()),
-            value=(float(df['家賃'].min()), float(df['家賃'].max())),
-            step=0.1,
+            max_value=50.0,
+            value=(float(df['家賃'].min()), 50.0),
+            step=1.0,
             format='%.1f'
         )
-        type_options = st.multiselect('■ 間取り選択', df['間取り'].unique(), default=df['間取り'].unique())
+        type_options = st.multiselect('■ 間取り選択', df['間取り'].unique(), default=['1K', '1LDK', '2LDK'])
         show_supermarkets = st.checkbox("スーパー", value=True)
         show_convenience_stores = st.checkbox("コンビニ", value=True)
         show_banks = st.checkbox("銀行", value=True)
@@ -202,24 +202,7 @@ def main():
     if st.session_state.get('search_clicked', False):
         m = create_map(st.session_state.get('filtered_df2', filtered_df2), show_supermarkets, supermarket_df, show_convenience_stores, convenience_store_df, show_banks, bank_df, show_cafes, cafe_df)
         folium_static(m)
-
-        # HTML for legend
-        legend_html = '''
-        <div style="position: fixed; 
-                    bottom: 50px; left: 50px; width: 150px; height: 150px; 
-                    border:2px solid grey; z-index:9999; font-size:14px;
-                    background-color:white;
-                    ">&nbsp; <b>Legend</b> <br>
-                      &nbsp; <i class="fa fa-map-marker fa-2x" style="color:green"></i>&nbsp; スーパー<br>
-                      &nbsp; <i class="fa fa-map-marker fa-2x" style="color:blue"></i>&nbsp; コンビニ<br>
-                      &nbsp; <i class="fa fa-map-marker fa-2x" style="color:red"></i>&nbsp; 銀行<br>
-                      &nbsp; <i class="fa fa-map-marker fa-2x" style="color:purple"></i>&nbsp; カフェ
-        </div>
-        '''
-
-        # Display legend in map
-        m.get_root().html.add_child(folium.Element(legend_html))
-
+    
         show_all_option = st.radio(
             "表示オプションを選択してください:",
             ('地図上の検索物件のみ', 'すべての検索物件'),
