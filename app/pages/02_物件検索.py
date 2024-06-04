3d46d18cff073f1fbebbaa0140efe2ca6f4b43fa
Jiromtrf <mtrfymd@gmail.com>
2024-06-04 19:27:37 +0900

    確認

--- app/pages/02_物件検索.py
+++ app/pages/02_物件検索.py
@@ -4,10 +4,10 @@ from google.oauth2.service_account import Credentials
 import pandas as pd
 import folium
 from streamlit_folium import folium_static
-from config import SPREADSHEET_DB_ID, PRIVATE_KEY_PATH, scopes
+from config import SPREADSHEET_DB_ID, scopes
 
 def get_credentials():
-    return Credentials.from_service_account_info(PRIVATE_KEY_PATH, scopes=scopes)
+    return Credentials.from_service_account_info(st.secrets["PRIVATE_KEY_PATH"], scopes=scopes)
 
 def load_data_from_gsheet(spreadsheet_id, sheet_name):
     creds = get_credentials()
